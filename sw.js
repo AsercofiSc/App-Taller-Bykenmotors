a2e03400ec5d33fe3180f4d46431cbd6600ba786
AsercofiSc <lordsaclar@hotmail.com>
2026-06-20 00:46:18 -0600

    version

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 ============================================= */
 
 // Cambiamos a v2 para forzar al móvil a borrar el caché viejo e instalar el nuevo
-const NOMBRE_CACHE = "taller-app-v43";
+const NOMBRE_CACHE = "taller-app-v44";
 
 const ARCHIVOS = [
   "./index.html",
